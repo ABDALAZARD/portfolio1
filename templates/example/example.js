f8abd805acacdab42a080e1d70441351b80a334a
ABDALAZARD <abdalazard@gmail.com>
2024-05-26 19:08:24 -0300

    Creating a new template

--- templates/example/example.js
+++ templates/example/example.js
@@ -1,10 +1,111 @@
+$(document).ready(function() {
 
-$('#buttonA').click(function(event) {
-    event.preventDefault();
-    console.log('You had voted for YES!');
-});
+    // Stacks
 
-$('#buttonB').click(function(event) {
-    event.preventDefault();
-    console.log('You had voted for NO!');
+    $('#PHP').on('mouseenter', function() {
+        console.log('Mouse sobre PHP');
+        $('#PHP').css('background-color', 'black')
+        .css('color','white')
+
+        $('#Portfolio_Creator').css('background-color', 'black')
+        .css('color','white')
+        
+    }).on('mouseleave', function() {
+        console.log('Mouse fora de PHP');
+        $('#PHP').css('background-color', 'white')
+        .css('color','black')
+        
+        $('#Portfolio_Creator').css('background-color', 'white')
+        .css('color','black')         
+    });
+    $('#Laravel').on('mouseenter', function() {
+        console.log('Mouse sobre Laravel');
+        $('#Laravel').css('background-color', 'black')
+        .css('color','white')
+        
+        // Dentro da ferramenta Laravel, lado profile
+        
+    }).on('mouseleave', function() {
+        console.log('Mouse fora de Laravel');
+        $('#Laravel').css('background-color', 'white')
+        .css('color','black')
+        
+        // Dentro da ferramenta Laravel, lado profile
+        
+    });
+    $('#JQuery').on('mouseenter', function() {
+        console.log('Mouse sobre JQuery');
+        $('#JQuery').css('background-color', 'black')
+            .css('color','white')
+            
+        $('#Portfolio_Creator').css('background-color', 'black')
+        .css('color','white')
+        
+        // Dentro da ferramenta JQuery, lado profile
+        
+    }).on('mouseleave', function() {
+        console.log('Mouse fora de JQuery');
+        $('#JQuery').css('background-color', 'white')
+        .css('color','black')
+        
+        $('#Portfolio_Creator').css('background-color', 'white')
+        .css('color','black')        
+    });
+    $('#React').on('mouseenter', function() {
+        console.log('Mouse sobre React');
+        $('#React').css('background-color', 'black')
+        .css('color','white')
+        
+        // Dentro da ferramenta React, lado profile
+        
+    }).on('mouseleave', function() {
+        console.log('Mouse fora de React');
+        $('#React').css('background-color', 'white')
+        .css('color','black')
+        
+        // Dentro da ferramenta React, lado profile
+        
+    });
+    $('#React-Native').on('mouseenter', function() {
+        console.log('Mouse sobre React-Native');
+        $('#React-Native').css('background-color', 'black')
+        .css('color','white')
+        
+        // Dentro da ferramenta React-Native, lado profile
+        
+    }).on('mouseleave', function() {
+        console.log('Mouse fora de React-Native');
+        $('#React-Native').css('background-color', 'white')
+        .css('color','black')
+        
+        // Dentro da ferramenta React-Native, lado profile
+        
+    });
+
+
+
+    // Projetos
+    $('#Portfolio_Creator').on('mouseenter', function() {
+
+        $('#Portfolio_Creator').css('background-color', 'black')
+        .css('color','white')
+
+        $('#PHP').css('background-color', 'black')
+        .css('color','white')
+        
+        $('#JQuery').css('background-color', 'black')
+        .css('color','white')
+
+    }).on('mouseleave', function() {
+        console.log('Mouse fora de Portfolio Creator');
+        $('#Portfolio_Creator').css('background-color', 'white')
+        .css('color','black')
+
+        $('#PHP').css('background-color', 'white')
+        .css('color','black')
+
+        $('#JQuery').css('background-color', 'white')
+        .css('color','black')
+                
+    });
 });
--- templates/example/example.php
+++ templates/example/example.php
@@ -1,17 +1,42 @@
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col s8">
-                <h5 class="title" id="title">Example Template</h5>
+        <div class="row templateBody">
+            <div class="col s6 profile" id="profile">
+                <div class="wrapper">
+                    <div class="inner" data-onvisible-trigger="1">
+                        <div class="foto">
+                            <span class="frame">
+                                <img class="foto" loading="lazy" src="<?php echo '../../../' . $profile['profile']; ?>"
+                                    alt="Foto" />
+                            </span>
+                        </div>
+                        <h2 id="text05" class="presentation"><?php echo $profile['title']; ?></h2>
+                        <!-- <p id="text13" class="subtitle"><?php echo $profile['subtitle']; ?></p> -->
+
+                        <div class="stacks-dropdown">
+                            <ul class="stacks">
+                                <?php
+                                    $stacks = explode(' | ', $profile['subtitle']);
+                                    foreach ($stacks as $stack) {
+                                        echo "<li style='margin-bottom:10px'><a id='$stack' class='stack1' href='#'>$stack</a></li>";
+                                    }
+                                ?>
+                            </ul>
+                            <ul class="frameworks">
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col s6">
-                    <submit class="btnA" id="buttonA">click for yes</submit>
-                </div>
-                <div class="col s6">
-                    <submit class=" btnB" id="buttonB">click for NO</submit>
-                </div>
+            <div class="col s6 professional" id="professional">
+                <!-- Conteúdo para ser preenchido -->
+                <p id="subtitle">Projects:</p>
+                <ul>
+                    <?php foreach ($projects as $project) { 
+                        $id = str_replace(' ', '_', $project['project_name']);
+                        echo "<li style='margin-bottom:10px'><a id='$id' class='stack1' href='#'>".$project['project_name']."</a></li>";
+                    }?>
+                </ul>
             </div>
         </div>
     </div>
